5ac73efb0c46ddaefc042369be3af57e4d66dcec
Adrian Christensen <adri0477@gmail.com>
2020-11-06 08:47:58 +0100

    w and h is set to window size

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
 // <reference path="./p5.global-mode.d.ts" />
 // Ignore the line above. It is for vscode p5 integration.
 
-const w = 600;
-const h = 600;
+let w = 500;
+let h = 500;
 
 //Set framerate var
 const fR = 60;
 
 //Background color of the main view
-const backgroundColor = [200];
+const backgroundColor = [0];
+
+function preload() {
+	w = windowWidth - 5;
+	h = windowHeight - 5;
+}
 
 function setup() {
 	//Create main canvas which the user sees
