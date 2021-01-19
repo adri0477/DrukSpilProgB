c232c3bf2972a06b0b4cc2d3755a9ae30f65d310
ChR1st0J <christofferjensenbusiness@gmail.com>
2021-01-19 09:28:02 +0100

    Update classes.js

--- classes.js
+++ classes.js
@@ -8,37 +8,31 @@ class customButton {
 		this.img = img;
 	}
 	display() {
-		//Set fill color to white
+
 		fill(255);
 
-		//Display box that forms the button
+
 		rect(this.x, this.y, this.w, this.h);
 
-		//Display image in center of box if there is an image
+
 		this.img ? image(this.img, this.x, this.y, this.w, this.h) : false;
 
-		//Set fill to black so text can be seen
+
 		fill(0);
 
-		//Set textAlign to center, center and display the text for the button
+
 		textAlign(CENTER, CENTER);
 		text(this.text, this.x + this.w / 2, this.y + this.h / 2);
 
-		//Set textAlign to default
+
 		textAlign(LEFT, BASELINE);
 	}
 	checkPress() {
-		//Set X and Y vars to mouse location
+
 		const X = mouseX;
 		const Y = mouseY;
 
-		/*
-		If mouse xpos and ypos is larger than x and y of the button. 
-		and mouse xpos and ypos aren't larger than x + width and y + height of the button. 
-		Then the mouse is inside the button. 
-		If the mouse is inside the button return true. 
-		Else return false
-		*/
+
 
 		if (
 			X >= this.x &&
