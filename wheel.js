18048e0be801f6095d314a6cd3af1948b23a531c
ChR1st0J <christofferjensenbusiness@gmail.com>
2021-01-19 09:28:09 +0100

    Update wheel.js

--- wheel.js
+++ wheel.js
@@ -1,16 +1,3 @@
-/**
- * Wheel Of Fortune
- * Çarkıfelek
- * v.0
- *
- * author: Abdulsamet Şahin
- *
- * ivme(acceleration): çarkın dönüş hızı, giderek azalıyor bu sayede çark başta hızlı sona doğru yavaş dönüyor.
- * pies: array, dilimleri içerir
- * count: dilim sayısı
- * angle: her bir dilimin, bir kenarı ile diğer kenarı arasındaki iç açısı
- */
-
 let count = 6;
 let angle = 360 / count;
 ivme = 0;
@@ -20,8 +7,6 @@ pies = [];
 /**
  *
  * @param {Pie} winner
- * EN: It only runs at the end of the spinning
- * TR: Çarkın dönüşü bittiğinde çalışır.
  */
 function onFinished(winner) {
 	console.log('winner -> ', winner);
@@ -34,17 +19,11 @@ function onFinished(winner) {
 /**
  *
  * @param {numeric} ivme
- * EN: It only runs when the spinning starts.
- * TR: Çark dönmeye başladığında çalışır.
  */
 function onStarted(ivme) {
 	console.log('Started');
 }
 
-/**
- * EN: Start spinning again.
- * TR: Tekrar döndür.
- */
 function spin() {
 	if (ivme > 0) {
 		return {
@@ -68,30 +47,17 @@ function doubleClicked() {
 function setupWheel() {
 	//createCanvas(displayWidth, displayHeight);
 
-	/**
-	 * To rotate things by using degrees.
-	 */
 	angleMode(DEGREES);
 
-	/**
-	 * Calculates the acceleration.
-	 * +1 means, we don't want to get 0 from the random func.
-	 * 4 can be changed. Its for speed.
-	 */
 	ivme = (random(4) + 1) / 10;
 	for (i = 0; i < count; i++) {
-		/**
-		 * We create our pies
-		 */
 
 		pies.push(new Pie(i, 'Drik ' + i + ' slurke', 0, 135, 62));
 	}
 }
 //let f = [red, green, 31231];
 
-/**
- * This function is a loop.
- */
+
 function drawWheel() {
 	/////////////////////////
 	if (frameCount <= 1) {
@@ -112,23 +78,22 @@ function drawWheel() {
 	}
 
 	fill(214, 0, 28);
-	// Center circle
+
 	ellipse(0, 0, 120, 120);
-	// The stick.
+
 	triangle(0, -180, -10, -240, 10, -240);
 
-	// decrement the acceleration every loop to make wheel stop.
 	ivme -= 0.001;
 
-	// if the acceleration is smaller then 1, we must to break the loop.
+
 	if (ivme <= 0) {
-		// breaks the loop
+
 		noLoop();
-		// refresh framecount
+
 		frameCount = 0;
-		// detect the winner
+
 		pies.forEach((pie) => {
-			// our stick is pointing 270
+
 			if (pie.aralik[0] % 360 <= 270 && pie.aralik[1] % 360 >= 270) {
 				onFinished(pie);
 			}
@@ -153,16 +118,12 @@ function Pie(order, a, r, g, b) {
 	this.g = g;
 	this.b = b;
 
-	/**
-	 * Spinning.
-	 */
+
 	this.move = function () {
 		this.order += ivme;
 	};
 
-	/**
-	 * Displays the pies with new calculated starting and ending points (angles).
-	 */
+
 	this.display = function () {
 		push();
 		fill(this.r, this.g, this.b);
@@ -173,9 +134,6 @@ function Pie(order, a, r, g, b) {
 		pop();
 	};
 
-	/**
-	 * Displays the texts.
-	 */
 	this.write = function () {
 		push();
 		fill(255);
